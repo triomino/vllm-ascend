e495c8a633a5ad144f0233a4cb836cac9911950d
Icey <1790571317@qq.com>
2026-01-19 09:28:07 +0800

    [Fusion] [Graph]Add Matmul Allreduce Rmsnorm fusion Pass (#5034)
    This PR add `MatmulAllreduceRmsnorm` operator and introduces a graph
    fusion pass for `matmul_allreduce_rmsnorm` operations. The
    implementation includes a new configuration flag, a pattern matching
    pass using `torch._inductor.pattern_matcher`.
    
    Co-authored-by: Trunrain [270250579@qq.com](mailto:270250579@qq.com)
    
    - vLLM version: v0.12.0
    - vLLM main:
    https://github.com/vllm-project/vllm/commit/ad32e3e19ccf0526cb6744a5fed09a138a5fb2f9
    
    ---------
    
    Signed-off-by: wxsIcey <1790571317@qq.com>
    Signed-off-by: tongrunze <t00574058@china.huawei.com>
    
    [bugfix]nd to nz optimize
    1. adapt to allreduce_rmsnorm_fusion_pass
    2. nd 2 nz

--- vllm_ascend/patch/worker/patch_distributed.py
+++ vllm_ascend/patch/worker/patch_distributed.py
@@ -111,5 +111,10 @@ class GroupCoordinatorPatch(GroupCoordinator):
                                                    gather_dim, scatter_sizes,
                                                    gather_sizes)
 
+    def all_reduce(self, input_):
+        if self.world_size == 1:
+            return input_
+        return torch.ops.vllm.all_reduce(input_, group_name=self.unique_name)
+
 
 vllm.distributed.parallel_state.GroupCoordinator = GroupCoordinatorPatch
